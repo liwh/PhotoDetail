fde51d77890bbd8770c861715d28dee640c5a10a
liwh <liwh87@gmail.com>
2012-01-11 14:50:48 +0800

    Initial Commit

---
+++ PhotoFlick/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhotoFlick
+//
+//  Created by liwh on 12-1-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
